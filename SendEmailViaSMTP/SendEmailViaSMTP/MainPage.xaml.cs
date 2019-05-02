e6e849b539495b05c08cd872dcdea26d7309eeee
Govardhan N <govardhan280890@gmail.com>
2019-05-02 10:40:56 +0530

    Alert message

--- SendEmailViaSMTP/SendEmailViaSMTP/MainPage.xaml.cs
+++ SendEmailViaSMTP/SendEmailViaSMTP/MainPage.xaml.cs
@@ -49,22 +49,22 @@ namespace SendEmailViaSMTP
         }
 
         static bool mailSent = false;
-        private static void SendCompletedCallback(object sender, AsyncCompletedEventArgs e)
+        private void SendCompletedCallback(object sender, AsyncCompletedEventArgs e)
         {
             // Get the unique identifier for this asynchronous operation.
             String token = (string)e.UserState;
 
             if (e.Cancelled)
             {
-                Console.WriteLine("[{0}] Send canceled.", token);
+                DisplayAlert("Alert", token+" Send canceled.", "Ok");
             }
             if (e.Error != null)
             {
-                Console.WriteLine("[{0}] {1}", token, e.Error.ToString());
+                DisplayAlert("Error", token+" "+ e.Error.ToString(),"Ok");
             }
             else
             {
-                Console.WriteLine("Message sent.");
+                DisplayAlert("Success","Message sent.","Ok");
             }
             mailSent = true;
         }
